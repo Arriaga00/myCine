bd5c03be2a2e30011e2a9540f2f029c51ca749d0
andres arriaga <andresfelipearriaga@gmail.com>
2024-01-09 10:31:16 -0500

    responsive

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -10,11 +10,11 @@ function Logo (props){
 
 
     return (
-      <div className='flex gap-2 '>
-        <img className='w-12 h-12 cursor-pointer' src={Img} alt="logo" />
-        <p className='flex justify-center items-center text-xl font-bold cursor-pointer hover:scale-105 ease-in-out duration-300 ' onClick={inicio}>Popetas</p>
-        <span className='text-[#edaede] text-4xl mt-2'>!</span>
-      </div>
+<div className='flex gap-2 justify-center items-center sm:justify-start'>
+  <img className='w-12 h-12 cursor-pointer' src={Img} alt="logo" />
+  <p className='flex justify-center items-center text-xl font-bold cursor-pointer hover:scale-105 ease-in-out duration-300 ' onClick={inicio}>Popetas</p>
+  <span className='text-[#edaede] text-4xl mt-2 items-center'>!</span>
+</div>
     )
 }
 
